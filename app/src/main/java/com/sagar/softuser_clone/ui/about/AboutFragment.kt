1fb7f7da5ef30b872c9d0bf95bf68e95bbd757a6
ragas17 <ragasshrestha15@gmail.com>
2021-01-15 19:52:59 +0545

    layout design and added web link to `About` page

--- app/src/main/java/com/sagar/softuser_clone/ui/about/AboutFragment.kt
+++ app/src/main/java/com/sagar/softuser_clone/ui/about/AboutFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -12,20 +13,16 @@ import com.sagar.softuser_clone.R
 
 class AboutFragment : Fragment() {
 
-    private lateinit var aboutViewModel: AboutViewModel
+    private lateinit var loadWebPage: WebView
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        aboutViewModel =
-                ViewModelProvider(this).get(AboutViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_about, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        aboutViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        loadWebPage = root.findViewById(R.id.webView)
+        loadWebPage.loadUrl("https://softwarica.edu.np/about-softwarica/")
         return root
     }
 }
